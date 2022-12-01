d27ce888b1e45fd98d2d5ee21894a33166a948a5
tildeherbe <118419404+tildeherbe@users.noreply.github.com>
2022-11-30 22:28:44 -0500

    Create s.py

---
+++ s.py
@@ -0,0 +1,3 @@
+# I have no fucking idea what I'm doing.
+# Going to attempt to .... do scaffolding for AoC. Maybe.
+# If I figure out things that would make life easier along the way then i'll put it here.
